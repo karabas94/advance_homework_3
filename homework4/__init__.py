9bf96b5c76778498c4177b7a2d5f8c7a34d17100
karabas94 <alkar_94@ukr.net>
2023-02-20 18:45:19 +0200

    application updated for database

--- homework4/__init__.py
+++ homework4/__init__.py
@@ -4,8 +4,12 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    # create
+    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'homework4.sqlite'),
+    )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -22,5 +26,8 @@ def create_app(test_config=None):
 
     from . import firstpart
     app.register_blueprint(firstpart.bp)
+
+    from . import db
+    db.init_app(app)
 
     return app
